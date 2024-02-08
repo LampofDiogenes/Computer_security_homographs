644eaddc40dcc64ddc3fd15d53df94fc461ccc07
Kaleb Bradford <131017785+LampofDiogenes@users.noreply.github.com>
2024-02-08 15:56:58 -0700

    maybe functional canonization

--- finding_homographs.py
+++ finding_homographs.py
@@ -11,27 +11,46 @@ def remove_slashes(string_path):
     # idea: split up the string along the backslashes, and then remove anything that begins with a period
     
     
-    
-    character =  "\\"
-    
-    # string_list = string_path.split(character)
+    # determining how to split the string
+    backslash =  "\\"
+    slash = "/"
+    # keeping track of the index
+    loop_amount = 0
 
+    # for reference
+    print(f"Original path: {string_path}")
 
-    string_list =  [string+character for string in string_path.split() if string]
+    string_list = string_path.split(backslash)
+    print(f"split path: {string_list}")
 
-    
-    print(string_list)
-    for string in string_list:
-        if (string == "./" or string == "../"):
-            string_list.pop()
+    for item in string_list:
+        index = loop_amount
+
+        if (item == "." or item == ""):
+            string_list.pop(index)
+        
+        elif (item == ".."):
+            string_list.pop(index)
+            string_list.pop((index-1))
+        
+        loop_amount += 1
+
+    true_string = ""
+
+    for item in string_list:
+        true_string += item
+
+    return true_string
     
     
     
 def main():
+    print()
     print("input a file path")
-    path_string = input()
+    path_string = "C:\\users\..\\path.py" #input()
     # Console.Readline()
-    remove_slashes(path_string)
-
+    new_string = remove_slashes(path_string)
+    print(f"canonical string: {new_string}")
+    print()
 
 main()

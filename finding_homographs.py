9dca6855439d84edf40bdf3d59345038d8a584e9
Kaleb Bradford <131017785+LampofDiogenes@users.noreply.github.com>
2024-02-08 15:00:54 -0700

    file submission I have

---
+++ finding_homographs.py
@@ -0,0 +1,37 @@
+# requirements:
+# let the user input a file
+# let the user input a second file
+# compare file paths, canonize (reduce to most condensed filepath)
+# return a message confirming or denying that the paths are homographs.
+
+
+
+
+def remove_slashes(string_path):
+    # idea: split up the string along the backslashes, and then remove anything that begins with a period
+    
+    
+    
+    character =  "\\"
+    
+    # string_list = string_path.split(character)
+
+
+    string_list =  [string+character for string in string_path.split() if string]
+
+    
+    print(string_list)
+    for string in string_list:
+        if (string == "./" or string == "../"):
+            string_list.pop()
+    
+    
+    
+def main():
+    print("input a file path")
+    path_string = input()
+    # Console.Readline()
+    remove_slashes(path_string)
+
+
+main()

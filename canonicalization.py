4b6bd40bca7b45934550e72141699a2be9d71c1d
AdvancedEel850 <90942089+AdvancedEel850@users.noreply.github.com>
2024-02-10 10:43:40 -0700

    update

--- canonicalization.py
+++ canonicalization.py
@@ -1,22 +1,19 @@
-# requirements:
-# let the user input a file
-# let the user input a second file
-# compare file paths, canonize (reduce to most condensed filepath)
-# return a message confirming or denying that the paths are homographs.
-
-
-
-# canonicalization 
 def canonicalize(string_path, cwd):
     string_path = ''.join('/' if c == '\\' else c for c in string_path) # Replace all \ with / in the path the user gives.
+
     full_relative = string_path.lower() # convert the path to lowercase, and set it as the full relative.
+
     if "c:" not in string_path:
         full_relative = cwd + "/" + string_path # If the user did not give an absolute path, add the cwd to the front.
+
     list_of_strings = full_relative.split("/") # Split the full relative path into a list on every /
+
     for i in range(0, len(list_of_strings)):
         if '' in list_of_strings:
             list_of_strings.remove('') # takes out any extra spaces that were produced by excesive ////
+
     absolutepath = [] # setup
+
     for item in list_of_strings:
         if item == "~":
             pass # dont do anythin if a ~ is present, we are already in the directory.
@@ -27,11 +24,15 @@ def canonicalize(string_path, cwd):
             pass # nothing happens
         else:
             absolutepath.append(item) # add the directory into the absolutepath.
+
     absolutepath = "/".join(absolutepath) # adds the / back in between every item.
+
     return absolutepath # returns the path.
+
 def are_homographs(filepath1, filepath2, cwd):
     return canonicalize(filepath1, cwd) == canonicalize(filepath2, cwd) # checks if the canonicalized versions are the same.
-def main():
+
+def user_input():
     cwd = "c:/users/bob" # our cwd
     print(f"current working directory: {cwd}\n") # display it
     path_string = input("input a file path1: ") # get input
@@ -40,4 +41,18 @@ def main():
         print("The file paths are homographs :)")
     else:
         print("The file paths are not homographs :(")
-main()
+
+
+def main():
+    menu = True
+    while menu:
+        choice = str(input("1. Run test.\n2. Input two files.\nq. Quit.\nChoice: "))
+        if choice == '1':
+            pass
+        elif choice == '2':
+            user_input()
+        elif choice == 'q':
+            menu = False
+
+if __name__ == '__main__':
+    main()

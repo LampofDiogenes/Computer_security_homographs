b09e373c5a66fec6860abf3a0cfebfaf86a636f1
AdvancedEel850 <90942089+AdvancedEel850@users.noreply.github.com>
2024-02-15 15:15:39 -0700

    update

--- Sql_injection.py
+++ Sql_injection.py
@@ -2,9 +2,11 @@ def generate_sql_string(username, password):
     return f"SELECT * FROM users WHERE username = '{username}' AND password = '{password}'"
 
 def weak_mitigation(username, password):
+    # do weak mitigation
     return generate_sql_string(username, password)
 
 def strong_mitigation(username, password):
+    # do strong mitigation
     return generate_sql_string(username, password)
 
 def test_valid(function):
@@ -12,38 +14,29 @@ def test_valid(function):
     # test 1
     username = "camlybb123"
     password = "p@ssword"
-    expected = "SELECT * FROM users WHERE username = 'camlybb123' AND password = 'p@ssword'"
 
-    result = function(username, password)
-
-    assert expected == result
+    print(function(username, password))
 
     # test 2
     username = ""
     password = ""
-    expected = "SELECT * FROM users WHERE username = '' AND password = ''"
 
-    result = function(username, password)
+    print(function(username, password))
 
-    assert expected == result
 
     # test 3
     username = ""
     password = ""
-    expected = "SELECT * FROM users WHERE username = '' AND password = ''"
 
-    result = function(username, password)
+    print(function(username, password))
 
-    assert expected == result
 
     # test 4
     username = ""
     password = ""
-    expected = "SELECT * FROM users WHERE username = '' AND password = ''"
 
-    result = function(username, password)
+    print(function(username, password))
 
-    assert expected == result
 
 def test_tautology(function):
     pass
@@ -58,9 +51,10 @@ def test_comment(function):
     pass
 
 
-print("validity test running...")
+print("Test valid - no mitigation")
 test_valid(generate_sql_string)
+print("Test valid - weak mitigation")
 test_valid(weak_mitigation)
+print("Test valid - strong mitigation")
 test_valid(strong_mitigation)
-print("validity test passed.")
 

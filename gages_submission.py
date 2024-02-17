4c381697d5100e488af06f6f034ec26c15d76ee3
Gage Smith <114100318+midgitg1314@users.noreply.github.com>
2024-02-16 21:07:15 -0700

    added weak mitigation

---
+++ gages_submission.py
@@ -0,0 +1,11 @@
+def weak_mitigation(input_str):
+    sanitized_input = input_str.replace("'", "''")
+    return sanitized_input
+
+# Test weak mitigation function
+def test_weak_mitigation():
+    test_input = "user1'; DROP TABLE users; --"
+    sanitized_input = weak_mitigation(test_input)
+    print(sanitized_input)
+
+test_weak_mitigation()

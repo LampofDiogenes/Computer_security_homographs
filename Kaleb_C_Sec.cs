c7308f45e8a37e0586e3a9a5630fdedaf2725db1
Kaleb Bradford <131017785+LampofDiogenes@users.noreply.github.com>
2024-02-29 16:18:18 -0700

    better application

--- Kaleb_C_Sec.cs
+++ Kaleb_C_Sec.cs
@@ -1,59 +1,91 @@
 using System.Collections.Generic;
+using System.Text;
 
 
-{
-    public static class C#_Security_Demo
+public static class C_Security_Demo
     {
         private static string SanitizeData(string userData)
         {
             // declaring what characters are "Not Allowed"
-            List<> disAllowList = new List<>();
-            string[] disAllowChars = {'/', '\\', '--', ':'};
+            List<char> disAllowList = new List<char>();
+            char[] disAllowChars = {'/', '\\', '-', ':'};
+            disAllowList.Add('/');
+            disAllowList.Add('\\');
+            disAllowList.Add('-');
+            disAllowList.Add(':');
             
             // declaring what will be sent back
-            string returnString;
+            string returnString = "";
 
             //checking to see if any characters are banned
-            foreach(character in userData)
+            for(int i=0; i < userData.Length; i++)
             {
-                if(character in disAllowChars)
+                if(userData[i] == disAllowList[0] || userData[i] == disAllowList[1] || 
+                   userData[i] == disAllowList[2] || userData[i] == disAllowList[3])
                 {
                     return returnString;
                 }
                 else
                 {
-                    returnString += character;
+                    var input = userData[i];
+                    string stringInput = Char.ToString(input);
+                    returnString = returnString.Insert(returnString.Length, stringInput);
                 }
             }
             return returnString;
         }
-        private static string OtherSolution()
-        {
 
+        private static Boolean FindDifference(string file1, string file2)
+        {
+            string filetext1;
+            string filetext2;
+            var fileStream = new FileStream(file1, FileMode.Open, FileAccess.Read);
+            using (var streamReader = new StreamReader(fileStream, Encoding.Default))
+            {
+                filetext1 = streamReader.ReadToEnd();
+            }
+
+            var fileStream2 = new FileStream(file2, FileMode.Open, FileAccess.Read);
+            using (var streamReader = new StreamReader(fileStream2, Encoding.Default))
+            {
+                filetext2 = streamReader.ReadToEnd();
+            }
+
+            if (filetext1 == filetext2)
+            {
+                return true;
+            }
+
+            return false;
         }
         
-        
-        
-        private static void Main()
+        private static void Main() // running examples here
         {
-
-
-            // Sanitizing the data problem
-            var oldData = "fireball:--";
-            Console.Write("Before Data: ");
-            Console.Writeline(oldData);
-            var betterData = SanitizeData(oldData);
-            Console.Writeline("After Sanitizing: ");
-            Console.WriteLine(betterData);
-
-            // Other problem that needs to be solved
-
-
             
+            // Sanitizing the data problem
+            string oldData = "fireball:--";
+            Console.Write("Before Data: ");
+            Console.WriteLine(oldData);
+            string betterData = SanitizeData(oldData);
+            Console.Write("After Sanitizing: ");
+            Console.WriteLine(betterData);
+            Console.WriteLine();
+
+            // determining if two files differ so that updates can be determined
+            Console.WriteLine("------------");
+            Console.WriteLine();
+
+            string filepath = "test.txt";
+            string filepath2 = "test2.txt";
+            Boolean isSame = FindDifference(filepath, filepath2);
+            if (isSame == true)
+            {
+                Console.WriteLine("The Contents are the same. No update needed");
+            }
+
+            if (isSame == false)
+            {
+                Console.WriteLine("The contents differ. Update required");
+            }
         }
-    
-    
     }
-
-
-}
